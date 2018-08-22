a7dd3d76e81c012655525cad9f04bd8ece1d6f89
sakithanavod <sakithanavod1994@gmail.com>
2018-08-22 13:38:55 +0530

    Questions can be added to Database

--- se_flow/app/Http/Controllers/MessegesController.php
+++ se_flow/app/Http/Controllers/MessegesController.php
@@ -3,17 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
 
 class MessegesController extends Controller
 {
     public function submit(Request $request)
-    {
+    { 
     	$this->validate($request,[
     		'heading'=>'required',
     		'content'=>'required',
     		'tags'=>'required'	
     	]);
 
-    	return 'SUCCESS';
+    	//create a new question
+    	$question = new Question;
+    	$question->heading = $request->input('heading');
+    	$question->content = $request->input('content');
+    	$question->tags = $request->input('tags');
+
+    	//save question
+    	$question->save();
+
+    	//redirect
+    	return redirect('/');
+
     }
 }
