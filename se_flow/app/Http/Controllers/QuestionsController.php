4d4d2e8567f1e600f7afd5e0e0a14877b28eb1b4
unknown <nimashandk94@gmail.com>
2018-08-27 15:12:59 +0530

    -Question form

--- se_flow/app/Http/Controllers/QuestionsController.php
+++ se_flow/app/Http/Controllers/QuestionsController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\QuestionsRequest;
+use App\User;
 use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
-    public function create(){
+    public function add_questions(QuestionRequest $request)
+    {
+        $inputs = $request->input();
+        $title = $inputs["title"];
+        $ques = $inputs["question"];
+        $description = $inputs["description"];
+        
+        $question = new Question();
+        $question->question = $ques;
+        $question->description = $description;
+        $result = $qusetion->save();
 
+        if ($result) {
+            return redirect('/home');
+        } else {
+            return redirect('/register');
+        }
     }
 
     public function edit(){
--- se_flow/resources/views/AddQuestion.blade.php
+++ se_flow/resources/views/AddQuestion.blade.php
@@ -1,1 +1,16 @@
-test
+<html>
+<h2>Add Questions</h2>
+<form method="post" action="{{ url("add_questions") }}">
+    {{ csrf_field() }}
+    <label for="Title">Title</label>
+    <input type="text" id="first_name" name="first_name" value=""/>
+    <br>
+    <label for="question">Question</label>
+    <input type="text" id="last_name" name="last_name" value=""/>
+    <br>
+    <label for="description">Description</label>
+    <textarea id="username" name="username" value=""/></textarea>
+    <br>
+    <input type="submit" value="Submit">
+</form>
+</html>	
