d6db1d666f1bef38374a2680a6a27cebcc1d4809
unknown <nimashandk94@gmail.com>
2018-08-27 12:48:54 +0530

    -Questions controller

---
+++ se_flow/app/Http/Controllers/QuestionsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionsController extends Controller
+{
+    public function create(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+        
+    }
+
+    public function destroy(){
+        
+    }
+}
