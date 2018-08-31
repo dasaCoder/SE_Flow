82ec842cdd3c92a2c328721e629b6fb071193413
sakithanavod <sakithanavod1994@gmail.com>
2018-08-31 17:37:16 +0100

    Updated Interface

--- se_flow/routes/web.php
+++ se_flow/routes/web.php
@@ -7,7 +7,7 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group.
 |
 */
 
@@ -17,22 +17,30 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-//google
-Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
-Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
-
-//github
-Route::get('auth/{{provider}}', 'Auth\LoginController@redirectToProvider');
-Route::get('auth/{{provider}}/callback', 'Auth\LoginController@handleProviderCallback');
-
-Route::get('/addQuestion', function () {
-    return view('AddQuestion');
-});
-
-Route::post('/add_questions', 'QuestionsController@add_questions');
-
-Auth::routes();
+Route::get('/admin','AdminController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/blog/list','BlogController@getList')->name('blog.list');
+
+// need to authenticate
+
+Route::get('/blog/create',function (){
+    return view('blog.create');
+})->name('blog.create');
+
+Route::get('/blog/update/{id}','BlogController@toUpdate');
+
+
+Route::get('/blog/{id}', 'BlogController@getOne')->name('blog');
+
+Route::post('/blog','BlogController@create')->name('blog');
+
+Route::patch('/blog/{id}', 'BlogController@update');
+
+Route::delete('/blog/{id}','BlogController@destroy');
+
+
+Route::get('admin/report','ExportExcelController@index');
+Route::get('/report/download','ExportExcelController@excel')->name('export_excel.excel');
+Route::get('/report/blogs/download','ExportExcelController@excelBlogs')->name('export_blogs.excel');
